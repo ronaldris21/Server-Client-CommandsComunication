7f65a3e13259c841cfb4be9fea128da94d6f1fec
Darsen12370 <pablocano2002@gmail.com>
2023-01-05 18:38:43 +0100

    rmi cliente hecho

--- src/edu/ucam/serverRMI/ServerRMI.java
+++ src/edu/ucam/serverRMI/ServerRMI.java
@@ -1,12 +1,25 @@
 package edu.ucam.serverRMI;
 
+import java.rmi.Naming;
 
+import edu.ucam.domain.IFincasAdministrator;
+import edu.ucam.parcial2.Concesionario;
+import edu.ucam.parcial2.ConcesionarioImp;
 
 public class ServerRMI {
 
 	public ServerRMI()
 	{
+		try {			
+			System.setProperty("java.rmi.server.codebase","file:/C:/directorio/");
 		
+			IFincasAdministrator f = new FincasAdministrator();
+			
+			Naming.rebind("rmi://localhost:8080/ConcesionarioService", f);
+			
+	} catch (Exception e) {
+		System.out.println("Trouble: " + e);
+	}
 	}
 
 	public static void main(String args[])
--- src/module-info.java
+++ src/module-info.java
@@ -2,10 +2,11 @@
  * 
  */
 /**
- * @author ronal
+ * @author ronald
  *
  */
 module PracticaDAD1 {
 	requires java.desktop;
 	requires java.rmi;
+	exports edu.ucam.domain to java.rmi;
 }
